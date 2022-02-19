efcc86ffee068c2fad06aae2f4571f6e1ae6fab9
Василий Новиков <chief95@mail.ru>
2022-02-20 02:24:47 +0300

    Базовый свитч

--- Function_1/Function_1/Program.cs
+++ Function_1/Function_1/Program.cs
@@ -12,15 +12,47 @@ namespace Function_1
         {
             String[,] personName;
             String[] personPosition;
-            bool isWorking true;
+            bool isWorking = true;
+            char userChoice;
 
             while(isWorking)
             {
-                Console.WriteLine("Добро пожаловать в электронную картотеку!\nВыберите пункт меню:\n");
-                Console.WriteLine("Добавить досье");
-                Console.WriteLine("Вывести все досье");
-                Console.WriteLine("Удалить досье");
-                Console.WriteLine("Поиск по фамилии");
+                
+                Console.WriteLine("Добро пожаловать в электронную картотеку!\n");
+                Console.WriteLine("1. Добавить досье");
+                Console.WriteLine("2. Вывести все досье");
+                Console.WriteLine("3. Удалить досье");
+                Console.WriteLine("4. Поиск по фамилии");
+                Console.WriteLine("5. Выход\n");
+                Console.Write("Выберите пункт меню: ");
+                userChoice = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+
+                switch (userChoice)
+                {
+                    case '1':
+                        Console.WriteLine("Вы выбрали пункт 1");
+                        break;
+                    case '2':
+                        Console.WriteLine("Вы выбрали пункт 2");
+                        break;
+                    case '3':
+                        Console.WriteLine("Вы выбрали пункт 3");
+                        break;
+                    case '4':
+                        Console.WriteLine("Вы выбрали пункт 4");
+                        break;
+                    case '5':
+                        Console.WriteLine("Вы выбрали пункт 5");
+                        isWorking = false;
+                        break;
+                    default:
+                        Console.WriteLine("Нет такого пункта меню!");
+                        break;
+                }
+                    
+
+                Console.ReadLine();
             }
         }
     }
