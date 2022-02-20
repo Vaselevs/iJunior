aed650f4a4d32d7ee52e10c5e1bed7820ae960b8
Василий Новиков <chief95@mail.ru>
2022-02-20 20:44:18 +0300

    первый и второй пункт
    Доделать, проблема с массивами

--- Function_1/Function_1/Program.cs
+++ Function_1/Function_1/Program.cs
@@ -10,14 +10,14 @@ namespace Function_1
     {
         static void Main(string[] args)
         {
-            String[,] personName;
-            String[] personPosition;
+            String[,] personName = new String[0,1];
+            String[] personPosition = new string[0];
             bool isWorking = true;
             char userChoice;
 
-            while(isWorking)
+            while (isWorking)
             {
-                
+                Console.Clear();
                 Console.WriteLine("Добро пожаловать в электронную картотеку!\n");
                 Console.WriteLine("1. Добавить досье");
                 Console.WriteLine("2. Вывести все досье");
@@ -31,10 +31,10 @@ namespace Function_1
                 switch (userChoice)
                 {
                     case '1':
-                        Console.WriteLine("Вы выбрали пункт 1");
+                        AddProfile(personName, personPosition);
                         break;
                     case '2':
-                        Console.WriteLine("Вы выбрали пункт 2");
+                        AllProfileView(personName, personPosition);
                         break;
                     case '3':
                         Console.WriteLine("Вы выбрали пункт 3");
@@ -43,16 +43,60 @@ namespace Function_1
                         Console.WriteLine("Вы выбрали пункт 4");
                         break;
                     case '5':
-                        Console.WriteLine("Вы выбрали пункт 5");
+                        Console.WriteLine("Спасибо за использование нашей картотеки, до свидания!");
                         isWorking = false;
                         break;
                     default:
                         Console.WriteLine("Нет такого пункта меню!");
                         break;
                 }
-                    
+
 
                 Console.ReadLine();
+            }
+        }
+
+        static void AddProfile(String[,] personNameinFunction, String[] personPositioninFunction)
+        {
+            String personFirstName;
+            String personLastName;
+            String personPosition;
+
+            Console.Clear ();
+            Console.WriteLine("Создаём новую запись в картотеке.");
+            Console.Write("Введите фамилию: ");
+            personLastName = Console.ReadLine();
+            Console.Write("Введите имя и очество: ");
+            personFirstName = Console.ReadLine();
+            Console.Write("Введите должность:");
+            personPosition = Console.ReadLine();
+
+            String[,] personNameinFunctionTemp = new String[personNameinFunction.GetLength(0) + 1, personNameinFunction.GetLength(1)];
+            String[] personPositioninFunctionTemp = new String[personPositioninFunction.Length + 1];
+
+            for (int i = 0; i < personNameinFunction.GetLength(0); i++)
+            {
+                for (int j = 0; j < personNameinFunction.GetLength(1); j++)
+                {
+                    personNameinFunctionTemp[i, j] = personNameinFunction[i, j];
+                }
+                personPositioninFunctionTemp[i] = personPositioninFunction[i];
+            }
+
+            personNameinFunctionTemp[personNameinFunctionTemp.GetLength(0), personNameinFunctionTemp.GetLength(1)-1] = personLastName;
+            personNameinFunctionTemp[personNameinFunctionTemp.GetLength(0), personNameinFunctionTemp.GetLength(1)] = personFirstName;
+            personPositioninFunctionTemp[personPositioninFunctionTemp.Length - 1] = personPosition;
+
+            personPositioninFunction = personPositioninFunctionTemp;
+            personNameinFunction = personNameinFunctionTemp;
+        }
+        static void AllProfileView(String[,] personNameinFunction, String[] personPositioninFunction)
+        {
+            Console.Clear();
+            Console.WriteLine("Вывожу список всех досье:");
+            for (int i = 0; i < personNameinFunction.GetLength(0); i++)
+            {
+                Console.WriteLine($"{i}. {personNameinFunction[i, 0]} {personNameinFunction[i, 1]}. Должность: {personPositioninFunction[i]}");
             }
         }
     }
