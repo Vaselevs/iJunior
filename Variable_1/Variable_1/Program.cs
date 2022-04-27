7d450584f2dffd6f3b2acee83383326ba9cb7134
Василий Новиков <chief95@mail.ru>
2022-04-27 18:09:31 +0300

    Задача с выводом имени

---
+++ Variable_1/Variable_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variable_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String firstName = "Иван";
+            String lastName = "Абрамов";
+            Console.WriteLine(firstName + " " + lastName);
+            firstName = "Антон";
+            lastName = "Огурцов";
+            Console.WriteLine(firstName + " " + lastName);
+
+        }
+    }
+}
