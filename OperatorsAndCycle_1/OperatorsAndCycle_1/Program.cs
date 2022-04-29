ea1df8059db269292f3374103925ce592415211b
Василий Новиков <chief95@mail.ru>
2022-04-29 16:31:58 +0300

    Закончил задачу

--- OperatorsAndCycle_1/OperatorsAndCycle_1/Program.cs
+++ OperatorsAndCycle_1/OperatorsAndCycle_1/Program.cs
@@ -10,17 +10,19 @@ namespace OperatorsAndCycle_1
     {
         static void Main(string[] args)
         {
-            Random rand = new Random();
+            Random random = new Random();
+            int randomMin = 0;
+            int randomMax = 101;
             int sum = 0;
-            int dividerX = 3;
-            int dividerY = 5;
-            int number = rand.Next(0,101);
+            int divider1 = 3;
+            int divider2 = 5;
+            int number = random.Next(randomMin,randomMax);
             Console.WriteLine(number);
             Console.WriteLine();
 
             for (int i = 0; i <= number; i++)
             {
-                if(i % dividerX == 0 || i % dividerY == 0)
+                if(i % divider1 == 0 || i % divider2 == 0)
                 {
                     sum += i;
                 }
---
+++ OperatorsAndCycle_2/OperatorsAndCycle_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAndCycle_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomNumberMin = 1;
+            int randomNumberMax = 28;
+            int number = random.Next(randomNumberMin, randomNumberMax);
+            int countOfNumber = 0;
+
+            Console.WriteLine($"Число - {number}");
+
+            for (int i = 0; i < System.Int32.MaxValue; i += number)
+            {
+                countOfNumber++;
+            }
+
+            Console.WriteLine($"Количество кратных чисел: {countOfNumber}");
+        }
+    }
+}
