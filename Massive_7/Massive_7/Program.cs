3504853456efd3debb78e6605fb6564cb061c851
Василий Новиков <chief95@mail.ru>
2022-05-08 15:42:28 +0300

    Все задачки про массивы

---
+++ Massive_7/Massive_7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Massive_7
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string targetString = "Мама мыла раму.";
+            string[] resultArray = targetString.Split(' ');
+
+            Console.WriteLine(targetString);
+
+            foreach(string tempString in resultArray)
+            {
+                Console.WriteLine(tempString);
+            }
+        }
+    }
+}
---
+++ Massive_8/Massive_8/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Massive_8
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Random random = new Random();
+            int minRandomNumber = 0;
+            int maxRandomNumber = 10;
+            int[] array = new int[10];
+            int userDisplacement = 3;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(minRandomNumber, maxRandomNumber);
+                Console.Write(array[i] + " ");
+            }
+
+            for(int i = 0; i<userDisplacement; i++)
+            {
+                int tempNumber = array[0];
+
+                for(int j = 0; j < array.Length - 1; j++)
+                {
+                    array[j] = array[j + 1];
+                }
+
+                array[array.Length-1] = tempNumber;
+            }
+
+            Console.WriteLine();
+            foreach (int number in array)
+                Console.Write(number + " ");
+        }
+    }
+}
