c0e8d8cf36927e86c3a2bb527d2fbe6471633c66
Василий Новиков <chief95@mail.ru>
2022-05-14 11:32:10 +0300

    инициализировал вторую задачу по функциям

---
+++ Function_2/Function_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Function_2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void StatusBar(int consoleX)
+        {
+
+        }
+    }
+}
