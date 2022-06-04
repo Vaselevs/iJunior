22a09173c42c35b36633753cbd8c112f1ae76f3c
Василий Новиков <chief95@mail.ru>
2022-06-04 20:42:14 +0300

    Обновление для 4-ой задачи по функциям
    Пересобрал проект на винде, под .NET 4.8

--- Function_4/Function_4/Program.cs
+++ Function_4/Function_4/Program.cs
@@ -11,21 +11,72 @@ namespace Function_4
     {
         static void Main(string[] args)
         {
+            bool isWorking = true;
+            string currentMap = "НЕ ВЫБРАНА НИ ОДНА КАРТА";
+            string[] mapsInDirectory;
+            string pathToMaps = "Maps/";
+            int userChoise;
+            int userMapChoise;
+            int cursorPositionForUserInputInMainMenu_Y = 2;
+            int cursorPositionForUserInputInMainMenu_X = 20;
+
+            while (isWorking)
+            {
+
+                Console.Clear();
+                Console.WriteLine("Приветствую вас в редакторе карт");
+                Console.WriteLine($"Ваша текущая карта: {currentMap}");
+                Console.WriteLine("Выберите пункт меню: ");
+                Console.WriteLine();
+                Console.WriteLine("1.Выбрать уже существующую карту");
+                Console.WriteLine("2.Нарисовать новую карту");
+                Console.WriteLine("3.Играть в выбранную карту");
+                Console.WriteLine("4.Выход из программы");
+
+                Console.SetCursorPosition(cursorPositionForUserInputInMainMenu_X, cursorPositionForUserInputInMainMenu_Y);
+                userChoise = System.Convert.ToInt32(Console.ReadLine());
 
 
-            
+                switch (userChoise)
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("Выберите номер карты из списка: ");
+                        mapsInDirectory = Directory.GetFiles(pathToMaps);
+                        
+                        for(int i = 0; i < mapsInDirectory.Length; i++)
+                        {
+                            Console.WriteLine($"{i+1}.{mapsInDirectory[i]}");
+                        }
 
+                        Console.ReadLine();
+                        break;
+                    case 2:
 
+                        PaintMap();
 
+                        break;
+                    case 3:
+
+                        break;
+                    case 4:
+                        isWorking = false;
+                        break;
+                    default:
+
+                        break;
+                }
+            }
         }
 
         static void PaintMap()
         {
             bool isPainting = true;
             string newMapName;
-            string[] map;
+            char[,] map;
             int mapHeight, mapWidth;
 
+            Console.Clear();
             Directory.CreateDirectory("Maps");
             Console.Write("Введите название новой карты: ");
             newMapName = Console.ReadLine();
@@ -34,7 +85,7 @@ namespace Function_4
             Console.Write("Введите ширину карты: ");
             mapWidth = Convert.ToInt32(Console.ReadLine());
             
-            map = new string[mapHeight];
+            map = new char[mapHeight,mapWidth];
             
 
             for(int i = 0; i < mapHeight; i++)
@@ -42,7 +93,7 @@ namespace Function_4
                 if (i == 0 || i == mapHeight - 1)
                 {
                     for (int j = 0; j < mapWidth; j++)
-                        map[i][j] = '#';
+                        map[i,j] = '#';
                 } else
                 {
                     map[i, 0] = '#';
@@ -61,7 +112,7 @@ namespace Function_4
 
             */
 
-            File.CreateText($"Maps/{newMapName}");
+            File.WriteAllText($"Maps/{newMapName}",Convert.ToString(map), Encoding.UTF8);
 
 
         }
@@ -81,9 +132,16 @@ namespace Function_4
             return map;
         }
 
-        static void PlayerGameplay()
+        static void PlayerGameplay(char[,] map)
         {
-
+            for(int i = 0; i < map.GetLength(0); i++)
+            {
+                for(int j = 0; j < map.GetLength(1); j++)
+                {
+                    Console.Write(map[i, j]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
