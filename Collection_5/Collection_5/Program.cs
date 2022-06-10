2a038807c98d04170cf2902479efc7999c651845
Василий Новиков <chief95@mail.ru>
2022-06-10 20:56:14 +0300

    Задачи по коллекциям
    Выполненная четвёртая задача, а так же начало 5-ой задача

---
+++ Collection_5/Collection_5/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfElementsInList = 10;
+
+            List<int> list1 = new List<int>(numberOfElementsInList);
+            List<int> list2 = new List<int>(numberOfElementsInList);
+
+            FillingList(ref list1);
+            FillingList(ref list2);
+
+            ShowList(list1);
+            ShowList(list2);
+
+            Console.ReadLine();
+        }
+
+        static void FillingList(ref List<int> list)
+        {
+            Random random = new Random();
+            int randomMinValue = 0;
+            int randomMaxValue = 1000;
+
+            foreach (int item in list)
+            {
+                int randomNumber = random.Next(randomMinValue, randomMaxValue);
+                list.Add(item);
+            }
+            Console.ReadLine();
+        }
+
+        static void ShowList(List<int> list)
+        {
+            foreach(int item in list)
+            {
+                Console.Write(list[item] + " ");
+            }
+        }
+    }
+}
--- Collections_4/Collections_4/Program.cs
+++ Collections_4/Collections_4/Program.cs
@@ -10,6 +10,88 @@ namespace Collections_4
     {
         static void Main(string[] args)
         {
+            bool isPlaying = true;
+            string userInput;
+            Dictionary<string, string> companyEmployees = new Dictionary<string, string>();
+
+            while (isPlaying)
+            {
+                Console.Clear();
+                Console.WriteLine("Приветствую в картотеки сотрудников 2.0!\nВот доступные вам пункты меню:");
+                Console.WriteLine("1.Добавить досье");
+                Console.WriteLine("2.Вывести все досье");
+                Console.WriteLine("3.Удалить досье");
+                Console.WriteLine("4.Выход");
+
+                userInput = Console.ReadLine();
+
+                if (Int32.TryParse(userInput, out int result))
+                {
+                    Console.Clear();
+                    switch (result)
+                    {
+                        case 1:
+                            AddDosier(companyEmployees);
+                            break;
+                        case 2:
+                            ShowAllDosiers(companyEmployees);
+                            break;
+                        case 3:
+                            DeleteDosier(companyEmployees);
+                            break;
+                        case 4:
+                            isPlaying = false;
+                            break;
+                        default:
+                            Console.WriteLine("Такого пункта нет в меню!");
+                            break;
+                    }
+                }
+                Console.ReadLine();
+            }
+            Console.WriteLine("Спасибо что пользовались нашей картотекой! До свидания!");
+        }
+
+        static void AddDosier(Dictionary<string,string> companyEmployees)
+        {
+            string nameOfEmploye;
+            string positionOfEmploye;
+            Console.Clear();
+            Console.Write("Введите ФИО сотрудника: ");
+            nameOfEmploye = Console.ReadLine();
+            Console.Write("Введите должность сотрудника: ");
+            positionOfEmploye = Console.ReadLine();
+            companyEmployees.Add(nameOfEmploye, positionOfEmploye);
+        }
+
+        static void ShowAllDosiers(Dictionary<string, string> companyEmployees)
+        {
+            int i = 1;
+            foreach(var employee in companyEmployees)
+            {
+                Console.WriteLine($"{i}. {employee.Key} - {employee.Value}");
+                i++;
+            }
+        }
+
+        static void DeleteDosier(Dictionary<string, string> companyEmployees)
+        {
+            string userInput;
+
+            Console.WriteLine("Введите ФИО удаляемого досье: ");
+
+            ShowAllDosiers(companyEmployees);
+
+            userInput = Console.ReadLine();
+
+            if (companyEmployees.ContainsKey(userInput))
+            {
+                companyEmployees.Remove(userInput);
+                Console.WriteLine($"Сотрудник {userInput} успешно удалён из картотеки!");
+            } else
+            {
+                Console.WriteLine($"Сотрудник {userInput} не найден! Попробуйте заново.");
+            }
         }
     }
 }
