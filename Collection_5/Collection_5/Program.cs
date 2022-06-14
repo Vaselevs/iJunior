559c2d00839d7a2c4f8d1648cca92dd76b86ee06
Василий Новиков <chief95@mail.ru>
2022-06-14 22:41:00 +0300

    5-ая задача по коллекциям и практика по ООП

--- Collection_5/Collection_5/Program.cs
+++ Collection_5/Collection_5/Program.cs
@@ -10,30 +10,33 @@ namespace Collection_5
     {
         static void Main(string[] args)
         {
-            int numberOfElementsInList = 10;
-
-            List<int> list1 = new List<int>(numberOfElementsInList);
-            List<int> list2 = new List<int>(numberOfElementsInList);
-
-            FillingList(list1);
-            FillingList(list2);
-
-            ShowList(list1);
-
-            ShowList(list2);
-
-            Console.ReadLine();
-        }
-
-        static void FillingList(List<int> list)
-        {
             Random random = new Random();
             int randomMinValue = 0;
-            int randomMaxValue = 1000;
+            int randomMaxValue = 10;
+            int numberOfElementsInList = 10;
 
-            for(int i = 0; i < list.Count; i++)
+            List<int> list1 = new List<int>();
+            List<int> list2 = new List<int>();
+            List<int> finallist = new List<int>();
+
+            FillingList(list1, numberOfElementsInList, random, randomMinValue, randomMaxValue);
+            FillingList(list2, numberOfElementsInList, random, randomMinValue, randomMaxValue);
+
+            ShowList(list1);
+            ShowList(list2);
+
+            MergerList(list1,list2, finallist);
+
+            ShowList(finallist);
+        }
+
+        static void FillingList(List<int> list, int numberOfElements, Random random, int randomMinValue, int randomMaxValue)
+        {
+            int randomNumber;
+
+            for (int i = 0; i < numberOfElements; i++)
             {
-                int randomNumber = random.Next(randomMinValue, randomMaxValue);
+                randomNumber = random.Next(randomMinValue, randomMaxValue);
                 list.Add(randomNumber);
             }
         }
@@ -43,6 +46,25 @@ namespace Collection_5
             for (int i = 0; i < list.Count; i++)
             {
                 Console.Write(list[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static void MergerList(List<int> firstList, List<int> secondList, List<int> finalList)
+        {
+            finalList.AddRange(firstList);
+            finalList.AddRange(secondList);
+
+            for(int i = 1; i < finalList.Count; i++)
+            {
+                for(int j = 0; j < i; j++)
+                {
+                    if (finalList[i] == finalList[j])
+                    {
+                        finalList.RemoveAt(i);
+                        i--;
+                    }
+                }
             }
         }
     }
---
+++ OOP_Practice_1/OOP_Practice_1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Practice_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Table[] tables = { new Table(1, 5), new Table(2, 10), new Table(3, 30)};
+
+            bool isOpen = true;
+
+            while (isOpen)
+            {
+                Console.WriteLine("Администрирование кафе\n");
+
+                for (int i = 0; i < tables.Length; i++)
+                {
+                    tables[i].ShowInfo();
+                }
+
+                isOpen = false;
+            }
+
+
+        }
+
+        class Table
+        {
+            private int _number;
+            private int _maxPlace;
+            private int _freePlace;
+
+            public Table(int number, int maxPlace)
+            {
+                _number = number;
+                _maxPlace = maxPlace;
+                _freePlace = maxPlace;
+            }
+
+            public Table()
+            {
+
+            }
+
+            public void ShowInfo()
+            {
+                Console.WriteLine($"Стол номер - {_number}. Кол-во мест - {_maxPlace}. Кол-во свободных мест - {_freePlace}");
+            }
+        }
+    }
+}
