c4d353824dce3da13e0594df3889510a13855aff
Василий Новиков <chief95@mail.ru>
2022-06-22 15:06:29 +0300

    1-ая задача по ООП

--- OOP_1/OOP_1/Program.cs
+++ OOP_1/OOP_1/Program.cs
@@ -1,13 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace OOP_1
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Player NewPlayer = new Player("Ivan", 100, 10);
+
+            NewPlayer.ShowInfo();
+        }
+    }
+
+    class Player
+    {
+        public string Name { get; private set; }
+        public int Health { get; private set; }
+        public int Mana { get; private set; }
+
+        public Player(string name, int health, int mana)
+        {
+            Name = name;
+            Health = health;
+            Mana = mana;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Игрока зовут - {Name}. У него {Health} здоровья и {Mana} маны.");
         }
     }
 }
-
