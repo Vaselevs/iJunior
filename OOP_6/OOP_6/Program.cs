cc4111422d25d12b1b0bf34764c14bcd87385a08
Василий Новиков <chief95@mail.ru>
2022-06-27 22:43:04 +0300

    6-ая задача по ООП

--- OOP_6/OOP_6/Program.cs
+++ OOP_6/OOP_6/Program.cs
@@ -12,5 +12,45 @@ namespace OOP_6
         {
 
         }
+
+        class Human
+        {
+            public string Name { get; private set; }
+            public string Description { get; private set; }
+            public int Money { get; private set; }
+            public List<Item> Inventory { get; private set; }
+
+            public Human(string name, string description, int money)
+            {
+                Name = name;
+                Description = description;
+                Money = money;
+            }
+        }
+
+        class Player : Human
+        {
+
+            public Player(string name, string description, int money) : base(name, description, money)
+            {
+
+            }
+
+        }
+
+        class Seller : Human
+        {
+            public Seller(string name, string description, int money) : base(name, description, money)
+            {
+
+            }
+        }
+
+        class Item
+        {
+            public string Name { get; private set; }
+            public int Cost { get; private set; }
+
+        }
     }
 }
