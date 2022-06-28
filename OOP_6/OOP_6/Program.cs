c9ad7c5951119ec2cba7e82d3f38bb6236c234a1
Василий Новиков <chief95@mail.ru>
2022-06-28 20:55:27 +0300

    апдейт по 6-ой задаче
    Разработка родительского класса Human а так же начало разработки Seller

--- OOP_6/OOP_6/Program.cs
+++ OOP_6/OOP_6/Program.cs
@@ -18,13 +18,22 @@ namespace OOP_6
             public string Name { get; private set; }
             public string Description { get; private set; }
             public int Money { get; private set; }
-            public List<Item> Inventory { get; private set; }
+            public List<Item> Inventory { get; protected set; }
 
             public Human(string name, string description, int money)
             {
                 Name = name;
                 Description = description;
                 Money = money;
+                Inventory = new List<Item>();
+            }
+
+            public void ShowInventory()
+            {
+                foreach(Item item in Inventory)
+                {
+                    item.Show();
+                }
             }
         }
 
@@ -33,7 +42,7 @@ namespace OOP_6
 
             public Player(string name, string description, int money) : base(name, description, money)
             {
-
+                Inventory = new List<Item>();
             }
 
         }
@@ -42,14 +51,53 @@ namespace OOP_6
         {
             public Seller(string name, string description, int money) : base(name, description, money)
             {
+                Inventory = new List<Item>();
+            }
+
+            public void SellItem(int buyingItemID)
+            {
+                if (buyingItemID < Inventory.Count && Inventory[buyingItemID].Cost < Money)
+                {
+                    Item sellingItem = Inventory[buyingItemID];
+                    Inventory.RemoveAt(buyingItemID);
+                    Console.WriteLine($"{Name} успешно продал товар {sellingItem.Name} за {sellingItem.Cost}");
+                }
+                else if (buyingItemID >= Inventory.Count)
+                {
+                    Console.WriteLine("Нет товара с таким номером!");
+                }
+                else
+                {
+                    Console.WriteLine("Покупателю не хватает денег!");
+                }
+            }
+
+            public void AddItemToSeller(string name, string cost)
+            {
+                Item userInputItem = new Item();
+
 
             }
+            
         }
 
         class Item
         {
             public string Name { get; private set; }
             public int Cost { get; private set; }
+
+            public Item(string name, int cost)
+            {
+                Name = name;
+                Cost = cost;
+            }
+
+            public void Show()
+            {
+                Console.WriteLine(Name + " - " + Cost);
+            }
+
+
 
         }
     }
