8be2fec18eb26c605eda5ff0af8286d5a594e50f
Василий Новиков <chief95@mail.ru>
2022-07-02 23:03:17 +0300

    прожолжение разработки 6-ой задачи по ООП

--- OOP_6/OOP_6/Program.cs
+++ OOP_6/OOP_6/Program.cs
@@ -12,104 +12,179 @@ namespace OOP_6
         {
 
         }
+    }
 
-        class Human
+    class Human
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public int Money { get; private set; }
+        public List<Item> Inventory { get; protected set; }
+
+        public Human(string name, string description, int money)
         {
-            public string Name { get; private set; }
-            public string Description { get; private set; }
-            public int Money { get; private set; }
-            public List<Item> Inventory { get; protected set; }
-
-            public Human(string name, string description, int money)
-            {
-                Name = name;
-                Description = description;
-                Money = money;
-                Inventory = new List<Item>();
-            }
-
-            public void ShowInventory()
-            {
-                foreach(Item item in Inventory)
-                {
-                    item.Show();
-                }
-            }
+            Name = name;
+            Description = description;
+            Money = money;
+            Inventory = new List<Item>();
         }
 
-        class Player : Human
+        public void ShowInventory()
         {
-
-            public Player(string name, string description, int money) : base(name, description, money)
+            foreach (Item item in Inventory)
             {
-                Inventory = new List<Item>();
+                item.Show();
             }
+        }
+    }
 
+    class Player : Human
+    {
+        public Player(string name, string description, int money) : base(name, description, money)
+        {
+            Inventory = new List<Item>();
         }
 
-        class Seller : Human
+        public void Game(Seller seller)
         {
-            public Seller(string name, string description, int money) : base(name, description, money)
-            {
-                Inventory = new List<Item>();
-            }
+            bool isPlaying = true;
+            string userInput;
 
-            public void SellItem(int buyingItemID)
+            while (isPlaying)
             {
-                if (buyingItemID < Inventory.Count && Inventory[buyingItemID].Cost < Money)
+                Console.WriteLine($"Имя игрока: {Name} | Кошелёк игрока: {Money}\n");
+                Console.WriteLine($"Игрок {Name} повстречал на своём пути продавца {seller.Name}");
+                Console.WriteLine($"Выберите пункт меню, для дальнейшего взаимодействия\n");
+                Console.WriteLine("1. Показать список предметов продовца");
+                Console.WriteLine("2. Показать ваш список предметов");
+                Console.WriteLine("3. Купить предмет у продовца");
+                Console.WriteLine("4. Добавить предмет продавцу");
+                Console.WriteLine("5. Уйти");
+
+                userInput = Console.ReadLine();
+
+                if(Int32.TryParse(userInput, out int userChoise))
                 {
-                    Item sellingItem = Inventory[buyingItemID];
-                    Inventory.RemoveAt(buyingItemID);
-                    Console.WriteLine($"{Name} успешно продал товар {sellingItem.Name} за {sellingItem.Cost}");
-                }
-                else if (buyingItemID >= Inventory.Count)
-                {
-                    Console.WriteLine("Нет товара с таким номером!");
+
                 }
                 else
                 {
-                    Console.WriteLine("Покупателю не хватает денег!");
-                }
-            }
-
-            public void AddItemToSeller(string name, string cost)
-            {
-                if (Int32.TryParse(cost, out int costInInt))
-                {
-                    Item userInputItem = new Item(name, costInInt);
-                    Inventory.Add(userInputItem);
-                    Console.WriteLine("Предмет успешно добавлен в инвентарь");
-                } 
-                else
-                {
-                    Console.WriteLine("Вы ввели неверную цену нового предмета");
+                    Console.WriteLine("Не верный формат ввода!");
                 }
 
-                
-
-
+                Console.ReadLine();
             }
+
             
+
+
         }
 
-        class Item
+        public void BuyItem(List<Item> sellerInventory)
         {
-            public string Name { get; private set; }
-            public int Cost { get; private set; }
 
-            public Item(string name, int cost)
+        }
+    }
+
+    class Seller : Human
+    {
+        public Seller(string name, string description, int money) : base(name, description, money)
+        {
+            Inventory = new List<Item>();
+        }
+
+        public void SellItem(int buyingItemID)
+        {
+            if (buyingItemID < Inventory.Count && Inventory[buyingItemID].Cost < Money)
             {
-                Name = name;
-                Cost = cost;
+                Item sellingItem = Inventory[buyingItemID];
+                Inventory.RemoveAt(buyingItemID);
+                Console.WriteLine($"{Name} успешно продал товар {sellingItem.Name} за {sellingItem.Cost}");
+            }
+            else if (buyingItemID >= Inventory.Count)
+            {
+                Console.WriteLine("Нет товара с таким номером!");
+            }
+            else
+            {
+                Console.WriteLine("Покупателю не хватает денег!");
+            }
+        }
+
+        public void CreateSellingItems()
+        {
+            bool isPlaying = true;
+
+            while (isPlaying)
+            {
+                ConsoleKeyInfo userInput;
+
+                Console.Clear();
+                Console.WriteLine("Нажмите пробел, что бы добавить новый предмет в инветарь");
+                Console.WriteLine("Нажмите Esc что бы выйти из режима добавления предметов продавцу\n");
+
+                userInput = Console.ReadKey();
+
+                switch (userInput.Key)
+                {
+                    case ConsoleKey.Spacebar:
+                        AddItem();
+                        break;
+                    case ConsoleKey.Escape:
+                        isPlaying = false;
+                        break;
+                    default:
+                        Console.WriteLine("Такой команды нет");
+                        break;
+                }
+            }
+        }
+
+        private void AddItem()
+        {
+            string name;
+            string cost;
+
+            Console.Write("Введите название нового предмета: ");
+
+            name = Console.ReadLine();
+
+            Console.Write("Введите стоимость нового предмета:");
+
+            cost = Console.ReadLine();
+
+            if (Int32.TryParse(cost, out int costInInt))
+            {
+                Item userInputItem = new Item(name, costInInt);
+                Inventory.Add(userInputItem);
+                Console.WriteLine("Предмет успешно добавлен в инвентарь");
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели неверную цену нового предмета");
             }
 
-            public void Show()
-            {
-                Console.WriteLine(Name + " - " + Cost);
-            }
 
 
 
+        }
+
+    }
+
+    class Item
+    {
+        public string Name { get; private set; }
+        public int Cost { get; private set; }
+
+        public Item(string name, int cost)
+        {
+            Name = name;
+            Cost = cost;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine(Name + " - " + Cost);
         }
     }
 }
