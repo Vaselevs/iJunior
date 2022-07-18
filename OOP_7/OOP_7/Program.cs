60bfe28d84ab3619c873772d64b86ed7156a9ee7
Василий Новиков <chief95@mail.ru>
2022-07-18 20:52:13 +0300

    доделал 7-ую задачу

--- OOP_7/OOP_7/Program.cs
+++ OOP_7/OOP_7/Program.cs
@@ -10,7 +10,10 @@ namespace OOP_7
     {
         static void Main(string[] args)
         {
+            Player player = new Player();
+            Game game = new Game();
 
+            game.Play(player);
         }
     }
 
@@ -18,28 +21,85 @@ namespace OOP_7
     {
         public void Play(Player player)
         {
-            Voyage activeVoyage;
+            Queue<Voyage> voyages = new Queue<Voyage>();
+            Voyage activeVoyage = null;
             bool isPlaying = true;
 
             while (isPlaying)
             {
                 Console.Clear();
-                Console.WriteLine($"Текущий рейс: {player.GetLastVoyage().PointOfDeparture} - {player.GetLastVoyage().PointOfArrival}");
-                Console.WriteLine($"Количество ещё необработанных рейсов - {player.GetAmountOfVoyages()}");
+                Console.WriteLine($"Текущий рейс: {ShowRoute()}");
+                Console.WriteLine();
+                Console.WriteLine("Выберите пункт меню: ");
+                Console.WriteLine("1. Создать новый маршрут");
+                Console.WriteLine("2. Посмотреть маршруты в очереди");
+                Console.WriteLine("3. Выйти из программы");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        voyages.Enqueue(player.AddNewVoyage());
+                        break;
+                    case "2":
+                        ShowAllVoyages();
+                        break;
+                    case "3":
+                        isPlaying = false;
+                        break;
+                    default:
+                        Console.WriteLine("Нет такой команды!");
+                        break;
+                }
+
+                if (activeVoyage == null || DateTime.Now > activeVoyage.GetFinishTime())
+                {
+                    if (voyages.Count > 0)
+                    {
+                        activeVoyage = voyages.Dequeue();
+                        activeVoyage.SetFinishTime();
+                    }
+                    else
+                    {
+                        activeVoyage = null;
+                    }
+                }
+            }
+
+            void ShowAllVoyages()
+            {
+                int voyageID = 1;
+                if(voyages.Count > 0)
+                {
+                    foreach (Voyage voyage in voyages)
+                    {
+                        Console.WriteLine($"{voyageID}. {voyage.ShowRoute()}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Список маршрутов пуст!");
+                }
+
+                Console.ReadLine();
+            }
+
+            string ShowRoute()
+            {
+                if (activeVoyage == null)
+                {
+                    return "Не задан текущий маршрут";
+                }
+                else
+                {
+                    return activeVoyage.ShowRoute();
+                }
             }
         }
     }
 
     public class Player
     {
-        private Queue<Voyage> voyageQueue;
-
-        public Player()
-        {
-            voyageQueue = new Queue<Voyage>();
-        }
-
-        public void AddNewVoyage()
+        public Voyage AddNewVoyage()
         {
             Console.Write("Введите название пункта отправления - ");
             string pointOfDeparture = Console.ReadLine();
@@ -48,19 +108,17 @@ namespace OOP_7
 
             Voyage newVoyage = new Voyage(pointOfDeparture, pointOfArrival);
 
-            voyageQueue.Enqueue(newVoyage);
-        }
+            Console.WriteLine();
 
-        public Voyage GetLastVoyage()
-        {
-            return voyageQueue.Peek();
-        }
+            newVoyage.CreateTheTrain();
 
-        public int GetAmountOfVoyages()
-        {
-            return voyageQueue.Count();
-        }
+            Console.Clear();
+            Console.WriteLine($"Отлично, вы создали новый маршрут: {newVoyage.ShowRoute()}");
+            Console.WriteLine("А теперь нажмите любую клавишу, что бы добавить маршрут в очередь!");
+            Console.Read();
 
+            return newVoyage;
+        }
     }
 
     public class Voyage
@@ -68,27 +126,22 @@ namespace OOP_7
         public string PointOfDeparture { get; private set; }
         public string PointOfArrival { get; private set; }
         public int Duration { get; private set; }
-        private Train train;
-        private DateTime finishTime;
-        private int minDurations = 0;
-        private int maxDurations = 5;
-        private int NumberOfPassengers;
-        private int minNumberOfPassengers = 10;
-        private int maxNumberOfPassengers = 500;
+        private Train _train;
+        private DateTime _finishTime;
+        private int _minMinuteDurations = 0;
+        private int _maxMinuteDurations = 5;
+        private int _NumberOfPassengers;
+        private int _minNumberOfPassengers = 10;
+        private int _maxNumberOfPassengers = 500;
 
         public Voyage(string pointOfDeparture, string pointOfArrival)
         {
             Random random = new Random();
-            train = new Train();
-            Duration = random.Next(minDurations, maxDurations);
-            NumberOfPassengers = random.Next(minNumberOfPassengers, maxNumberOfPassengers);
+            _train = new Train();
+            Duration = random.Next(_minMinuteDurations, _maxMinuteDurations);
+            _NumberOfPassengers = random.Next(_minNumberOfPassengers, _maxNumberOfPassengers);
             PointOfDeparture = pointOfDeparture;
             PointOfArrival = pointOfArrival;
-        }
-
-        public int GetNumberOfPassengers()
-        {
-            return NumberOfPassengers;
         }
 
         public void CreateTheTrain()
@@ -97,18 +150,20 @@ namespace OOP_7
 
             while (isCreating)
             {
+                Console.Clear();
+
                 ConsoleKey userInput;
 
-                Console.WriteLine($"Количество желающих поехать на этом маршруте - {NumberOfPassengers}");
-                Console.WriteLine($"Количество мест в поезде: {train.GetNumberOfPassengersSeats()}");
+                Console.WriteLine($"Количество желающих поехать на этом маршруте: {_NumberOfPassengers}");
+                Console.WriteLine($"Количество мест в поезде: {_train.GetNumberOfPassengersSeats()}");
                 Console.WriteLine("Вам надо создать поезд, вмещающий всех пассажиров");
                 Console.WriteLine("Что бы добвать вагон, нажмите Space");
 
-                userInput = Console.ReadKey().Key;
+                userInput = Console.ReadKey(true).Key;
                 
                 if(userInput == ConsoleKey.Spacebar)
                 {
-                    train.AddNewCarriage();
+                    _train.AddNewCarriage();
                 }
                 else
                 {
@@ -116,20 +171,43 @@ namespace OOP_7
                     Console.ReadLine();
                 }
 
-                Console.Clear();
+                if(_NumberOfPassengers <= _train.GetNumberOfPassengersSeats())
+                {
+                    isCreating = false;
+                }
             }
         }
 
-    }
+        public string ShowRoute()
+        {
+            if(PointOfDeparture == null && PointOfArrival == null)
+            {
+                return "МАРШРУТ НЕ СОЗДАН";
+            } 
+            else
+            {
+                return PointOfDeparture + " - " + PointOfArrival;
+            }
+        }
 
+        public void SetFinishTime()
+        {
+            _finishTime = DateTime.Now.AddMinutes(Duration);
+        }
+
+        public DateTime GetFinishTime()
+        {
+            return _finishTime;
+        }
+    }
 
     public class Train
     {
-        private List<PassengerCarriage> Carriages { get; set; }
+        private List<PassengerCarriage> _carriages;
 
         public Train()
         {
-            Carriages = new List<PassengerCarriage>();
+            _carriages = new List<PassengerCarriage>();
         }
 
         public void AddNewCarriage()
@@ -137,7 +215,7 @@ namespace OOP_7
             Console.Write("Введите количество мест в пассажирском вагоне: ");
             if(Int32.TryParse(Console.ReadLine(), out int numberOfSeats))
             {
-                Carriages.Add(new PassengerCarriage(numberOfSeats));
+                _carriages.Add(new PassengerCarriage(numberOfSeats));
             }
             else
             {
@@ -148,7 +226,7 @@ namespace OOP_7
         public int GetNumberOfPassengersSeats()
         {
             int numberOfSeats = 0;
-            foreach(PassengerCarriage Carriage in Carriages)
+            foreach(PassengerCarriage Carriage in _carriages)
             {
                 numberOfSeats += Carriage.NumberOfSeats;
             }
