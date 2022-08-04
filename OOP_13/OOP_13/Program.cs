2a539b051db25e5345007bde1fca97b81242e06d
Василий Новиков <chief95@mail.ru>
2022-08-04 22:55:08 +0300

    доработка 13-ой задачи по ООП

--- OOP_13/OOP_13/Program.cs
+++ OOP_13/OOP_13/Program.cs
@@ -20,17 +20,52 @@ namespace OOP_13
     {
         private CarService _carService;
         private Detail[] _details;
+        private Queue<Car> _cars;
+        private int _playerMoney;
 
         public Game()
         {
-            _details = new Detail[10] {new Detail("Карбюратор", 30), new Detail("Бампер", 20), new Detail("Корбка передач", 75), new Detail("Крыша", 35), new Detail("Колесо", 5), new Detail("Двигатель", 110),
-        new Detail("Шасси", 90), new Detail("Руль", 7), new Detail("Тормоза", 45), new Detail("Выхлоп", 12)};
+            _playerMoney = 1000;
+            int brokenCarCount = 15;
+            _cars = new Queue<Car>();
+            _details = new Detail[10]
+            {
+                new Detail("Карбюратор", 30),
+                new Detail("Бампер", 20),
+                new Detail("Корбка передач", 75),
+                new Detail("Крыша", 35),
+                new Detail("Колесо", 5),
+                new Detail("Двигатель", 110),
+                new Detail("Шасси", 90),
+                new Detail("Руль", 7),
+                new Detail("Тормоза", 45),
+                new Detail("Выхлоп", 12)
+            };
             _carService = new CarService(_details, 1000);
+
+            for (int i = 0; i < brokenCarCount; i++)
+            {
+                _cars.Enqueue(new Car(_details));
+            }
         }
 
         public void Play()
         {
+            Console.WriteLine("Добро пожаловать в мастерскую!");
+            Console.WriteLine($"У вас {_playerMoney} денег");
+            Console.WriteLine("На вашем складе есть следующие детали: ");
+            Console.WriteLine();
+
             _carService.ShowStorage();
+
+            Console.WriteLine();
+            Console.WriteLine("А так же у вас следующие сломаные машины в очереди:");
+
+            for(int i = 0; i < _cars.Count; i++)
+            {
+                Car car = _cars.Dequeue();
+                Console.WriteLine($"{i + 1}. У машины {i + 1} сломан - {car.GetBrokenDetailName()}");
+            }
         }
 
     }
@@ -44,17 +79,20 @@ namespace OOP_13
         public CarService(Detail[] _details, int money)
         {
             int maxDetailsCount = _details.Length;
+
             _random = new Random();
             _detailsStorage = new Dictionary<Detail, int>();
 
             for (int i = 0; i < _details.Length; i++)
             {
                 int detailsCount = _random.Next(maxDetailsCount);
+
                 if (detailsCount > 0)
                 {
                     _detailsStorage.Add(_details[i], detailsCount);
                 }
             }
+
             Money = money;
         }
 
@@ -62,7 +100,7 @@ namespace OOP_13
         {
             for(int i = 0; i < _detailsStorage.Count; i++)
             {
-                Console.WriteLine($"Наименование и цена: {_detailsStorage.ElementAt(i).Key.GetInfo()} | количество - {_detailsStorage.ElementAt(i).Value}");
+                Console.WriteLine($"Наименование: {_detailsStorage.ElementAt(i).Key.Name} | Цена: {_detailsStorage.ElementAt(i).Key.Price} | Количество: {_detailsStorage.ElementAt(i).Value}");
             }
         }
     }
@@ -75,30 +113,29 @@ namespace OOP_13
         public Car(Detail[] _details)
         {
             _random = new Random();
-
             _brokenDetail = _details[_random.Next(_details.Length)];
         }
 
-        public void GetBrokenDetailInfo()
+        public string GetBrokenDetailName()
         {
+            return _brokenDetail.Name;
+        }
 
+        public int GetBrokenDetailPrice()
+        {
+            return _brokenDetail.Price;
         }
     }
 
     public class Detail
     {
-        private string _name;
-        private int _price;
+        public string Name { get; private set; }
+        public int Price { get; private set; }
 
         public Detail(string name, int price)
         {
-            _name = name;
-            _price = price;
-        }
-
-        public string GetInfo()
-        {
-            return _name + " - " + _price;
+            Name = name;
+            Price = price;
         }
     }
 }
