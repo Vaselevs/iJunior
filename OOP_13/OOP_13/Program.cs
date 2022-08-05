f6b0cb68a12869b07fb07ef03e9e227824ecda2c
Василий Новиков <chief95@mail.ru>
2022-08-05 19:30:14 +0300

    Доделал 13 задачу

--- OOP_13/OOP_13/Program.cs
+++ OOP_13/OOP_13/Program.cs
@@ -21,12 +21,10 @@ namespace OOP_13
         private CarService _carService;
         private Detail[] _details;
         private Queue<Car> _cars;
-        private int _playerMoney;
 
         public Game()
         {
-            _playerMoney = 1000;
-            int brokenCarCount = 15;
+            int brokenCarCount = 20;
             _cars = new Queue<Car>();
             _details = new Detail[10]
             {
@@ -41,6 +39,7 @@ namespace OOP_13
                 new Detail("Тормоза", 45),
                 new Detail("Выхлоп", 12)
             };
+
             _carService = new CarService(_details, 1000);
 
             for (int i = 0; i < brokenCarCount; i++)
@@ -53,11 +52,11 @@ namespace OOP_13
         {
             bool isPlaying = true;
 
-            while (isPlaying)
+            while (isPlaying && _cars.Count > 0)
             {
                 Console.Clear();
                 Console.WriteLine("Добро пожаловать в мастерскую!");
-                Console.WriteLine($"У вас {_playerMoney} денег");
+                Console.WriteLine($"У вас {_carService.Money} денег");
 
                 Console.WriteLine("На вашем складе есть следующие детали: ");
                 Console.WriteLine();
@@ -65,35 +64,61 @@ namespace OOP_13
                 _carService.ShowStorage();
 
                 Console.WriteLine("К вам приехал клиент!");
+                Console.WriteLine();
 
                 Car car = _cars.Dequeue();
 
-                Console.WriteLine($"У машины клиента сломан - {car.GetBrokenDetailName()}, цена починки: {car.GetBrokenDetailPrice() + _carService.ServiceTax}");
+                Console.WriteLine($"У машины клиента сломан - {car.BrokenDetail.Name}, цена починки: {car.BrokenDetail.Price + _carService.ServiceTax}");
                 Console.WriteLine("Выберите что сделать с клиентом: ");
                 Console.WriteLine($"1. Отказать клиенту. Это будет вам стоить {_carService.ServicePenalty}");
-                Console.WriteLine($"2. Попытаться починить машину из деталей на складе. Вы получите награду в виде стоимости детали + {_carService.ServiceTax}");
+                Console.WriteLine($"2. Попытаться починить машину из деталей на складе. Вы получите награду {car.BrokenDetail.Price + _carService.ServiceTax}");
+                Console.WriteLine($"В случае установки не правильной детали, вам так же надо будет заплатить штраф: {_carService.ServicePenalty}");
+                Console.WriteLine($"Что бы выйти из прогрммы нажмите ENTER");
 
                 if(Int32.TryParse(Console.ReadLine(), out int userChoise))
                 {
                     switch (userChoise)
                     {
-                        case 1:
-                            RefuseClient();
-                            break;
                         case 2:
-
+                            RepairCar(car);
                             break;
                         default:
-
+                            RefuseClient();
                             break;
                     }
+                }
+                else
+                {
+                    isPlaying = false;
                 }
             }
 
             void RefuseClient()
             {
                 _carService.TakePenalty();
-                Console.WriteLine($"Вы отказали клиенту в обслуживании! Вы получаете штраф {_carService.ServiceTax}");
+                Console.WriteLine($"Вы не обслужили клиента! Вы получаете штраф {_carService.ServicePenalty}");
+            }
+
+            void RepairCar(Car car)
+            {
+                Console.Write("Выберите номер детали из склада выше: ");
+
+                if(Int32.TryParse(Console.ReadLine(), out int userChoise))
+                {
+
+                    if (_carService.GetDetailByIndex(userChoise-1).Name == car.BrokenDetail.Name && _carService.GetDetailCountByIndex(userChoise-1) > 0)
+                    {
+                        Console.WriteLine($"Вы успешно починили деталь и заработали {car.BrokenDetail.Price + _carService.ServiceTax}");
+
+                        _carService.AddReward(car.BrokenDetail.Price);
+                        _carService.DecreaseDetailCount(car.BrokenDetail.Name);
+                    }
+                    else
+                    {
+                        _carService.DecreaseDetailCount(_carService.GetDetailByIndex(userChoise - 1).Name);
+                        RefuseClient();
+                    }
+                }
             }
         }
     }
@@ -101,7 +126,6 @@ namespace OOP_13
     public class CarService
     {
         private Dictionary<Detail, int> _detailsStorage;
-        private Random _random;
         public int Money { get; private set; }
         public int ServiceTax { get; private set; }
         public int ServicePenalty { get; private set; }
@@ -112,17 +136,13 @@ namespace OOP_13
             ServicePenalty = 50;
             int maxDetailsCount = _details.Length;
 
-            _random = new Random();
+            Random random = new Random();
             _detailsStorage = new Dictionary<Detail, int>();
 
             for (int i = 0; i < _details.Length; i++)
             {
-                int detailsCount = _random.Next(maxDetailsCount);
-
-                if (detailsCount > 0)
-                {
-                    _detailsStorage.Add(_details[i], detailsCount);
-                }
+                int detailsCount = random.Next(maxDetailsCount);
+                _detailsStorage.Add(_details[i], detailsCount);
             }
 
             Money = money;
@@ -132,36 +152,51 @@ namespace OOP_13
         {
             for(int i = 0; i < _detailsStorage.Count; i++)
             {
-                Console.WriteLine($"Наименование: {_detailsStorage.ElementAt(i).Key.Name} | Цена: {_detailsStorage.ElementAt(i).Key.Price} | Количество: {_detailsStorage.ElementAt(i).Value}");
+                Console.WriteLine($"{i+1}. {_detailsStorage.ElementAt(i).Key.Name} | Цена: {_detailsStorage.ElementAt(i).Key.Price} | Количество: {_detailsStorage.ElementAt(i).Value}");
             }
         }
 
         public void TakePenalty()
         {
             Money -= ServicePenalty;
-            
+        }
+
+        public Detail GetDetailByIndex(int userChoise)
+        {
+            return _detailsStorage.ElementAt(userChoise).Key;
+        }
+
+        public int GetDetailCountByIndex(int userChoise)
+        {
+            return _detailsStorage.ElementAt(userChoise).Value;
+        }
+
+        public void AddReward(int tax)
+        {
+            Money += tax + ServiceTax;
+        }
+
+        public void DecreaseDetailCount(string detailName)
+        {
+            for(int i = 0; i < _detailsStorage.Count; i++)
+            {
+                if (detailName == _detailsStorage.ElementAt(i).Key.Name && _detailsStorage.ElementAt(i).Value > 0)
+                {
+                    _detailsStorage[_detailsStorage.ElementAt(i).Key]--;
+                    break;
+                }
+            }
         }
     }
 
     public class Car
     {
-        private Detail _brokenDetail;
-        private Random _random;
+        public Detail BrokenDetail { get; private set; }
 
         public Car(Detail[] _details)
         {
-            _random = new Random();
-            _brokenDetail = _details[_random.Next(_details.Length)];
-        }
-
-        public string GetBrokenDetailName()
-        {
-            return _brokenDetail.Name;
-        }
-
-        public int GetBrokenDetailPrice()
-        {
-            return _brokenDetail.Price;
+            Random random = new Random();
+            BrokenDetail = _details[random.Next(_details.Length)];
         }
     }
 
