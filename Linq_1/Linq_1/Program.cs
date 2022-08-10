653e3d98411e48b292e357807fae5cf665f894ae
Василий Новиков <chief95@mail.ru>
2022-08-10 23:16:03 +0300

    Ининциализация 1-ой задачи Linq

---
+++ Linq_1/Linq_1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
