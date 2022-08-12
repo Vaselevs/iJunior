95d88394144ab0161cdf6ededb76ddc9fcb28ead
Василий Новиков <chief95@mail.ru>
2022-08-12 21:08:55 +0300

    Доделал пятую задачу по LinQ

--- Linq_5/Linq_5/Program.cs
+++ Linq_5/Linq_5/Program.cs
@@ -10,14 +10,56 @@ namespace Linq_5
     {
         static void Main(string[] args)
         {
+            Game game = new Game();
 
+            game.Play();
         }
     }
 
-    public class Stotage
+    public class Game
     {
-        private CanOfStew _caOfStews;
+        private Storage _storage;
 
+        public Game()
+        {
+            _storage = new Storage();
+        }
+
+        public void Play()
+        {
+            ShowInfo(_storage.GetOverdueStew());
+
+            void ShowInfo(List<CanOfStew> cansOfStew)
+            {
+                foreach (CanOfStew canOfStew in cansOfStew)
+                {
+                    canOfStew.ShowInfo();
+                }
+            }
+        }
+    }
+
+    public class Storage
+    {
+        private List<CanOfStew> _cansOfStew;
+
+        public Storage()
+        {
+            _cansOfStew = new List<CanOfStew>()
+            {
+                new CanOfStew("Бурёнка", new DateTime(2015, 1, 1), new DateTime(2020, 1, 1)),
+                new CanOfStew("Солёнка", new DateTime(2013, 1, 1), new DateTime(2023, 1, 1)),
+                new CanOfStew("Харчо", new DateTime(2012, 1, 1), new DateTime(2019, 1, 1)),
+                new CanOfStew("Свинина", new DateTime(2016, 1, 1), new DateTime(2022, 1, 1)),
+                new CanOfStew("Марочная", new DateTime(2012, 1, 1), new DateTime(2015, 1, 1)),
+                new CanOfStew("Именная", new DateTime(2018, 1, 1), new DateTime(2026, 1, 1))
+            };
+        }
+
+        public List<CanOfStew> GetOverdueStew()
+        {
+            return _cansOfStew.Where(_canOfStews => _canOfStews.ExpirationDate.Year < DateTime.Now.Year).ToList();
+        }
     }
 
     public class CanOfStew
